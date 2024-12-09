93dcea7af5d8e34eeeb386acba2b812671fb887a
Rovshik <rovsh.dev@gmail.com>
2024-12-10 03:39:39 +0500

    shared prefs init

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'data/local/shared_refs.dart';
 import 'exercise_menu.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Prefs.init();
+
   runApp(const MyApp());
 }
 
