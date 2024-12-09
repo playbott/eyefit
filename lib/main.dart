b76e4a84f018d7423e8f536795a86b00277fb614
Rovshik <rovsh.dev@gmail.com>
2024-12-10 00:58:03 +0500

    add screen util initialization widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'exercise_menu.dart';
 
 void main() {
@@ -10,12 +11,20 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'EyeFit',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const ExerciseMenu(), // Подключаем меню упражнений.
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_ , child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'EyeFit',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: const ExerciseMenu(),
+        );
+      }
     );
   }
 }
