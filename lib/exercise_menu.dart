c4aae4d0e44acd32711cc6d3b18eb8c017481f0c
Rovshik <rovsh.dev@gmail.com>
2024-12-12 04:12:13 +0500

    add fps counter label to main screen

--- lib/exercise_menu.dart
+++ lib/exercise_menu.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'entities/exercise.dart';
 import 'exercises/diagonals.dart';
 import 'exercises/draw_circle.dart';
@@ -7,7 +6,7 @@ import 'exercises/look_up_down.dart';
 import 'exercises/pendulum.dart';
 import 'exercise_screen.dart';
 import 'exercises/rectangle.dart';
-
+import 'fps_label.dart';
 
 final exercises = [
   Exercise(
@@ -46,36 +45,41 @@ class ExerciseMenu extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Упражнения для глаз'),
       ),
-      body: Container(
-        constraints: const BoxConstraints(maxWidth: 600),
-        child: ListView.builder(
-          itemCount: exercises.length,
-          itemBuilder: (context, index) {
-            final exercise = exercises[index];
-            return Card(
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10),
-              ),
-              margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-              child: ListTile(
+      body: SafeArea(
+        child: Container(
+          constraints: const BoxConstraints(maxWidth: 600),
+          child: ListView.builder(
+            itemCount: exercises.length,
+            itemBuilder: (context, index) {
+              final exercise = exercises[index];
+              return Card(
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(10),
                 ),
-                title: Text(exercise.title),
-                subtitle: Text(exercise.description),
-                trailing: const Icon(Icons.arrow_forward),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) =>
-                          ExerciseScreen(content: exercise.widget, title: exercise.title),
-                    ),
-                  );
-                },
-              ),
-            );
-          },
+                margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+                child: ListTile(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  title: Text(exercise.title),
+                  subtitle: Text(exercise.description),
+                  trailing: const Icon(Icons.arrow_forward),
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => FpsLabel(
+                          child: ExerciseScreen(
+                              content: exercise.widget,
+                              title: exercise.title),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              );
+            },
+          ),
         ),
       ),
     );
